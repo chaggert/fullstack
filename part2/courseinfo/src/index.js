0eb957f893979cb7c9514a0ce90c770946989915
Chris <Chris.haggert@gmail.com>
2019-10-02 18:32:17 -0400

    finished 2.3 2.4 and 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,35 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Course from "./components/Course";
-// import Header from "./components/Header";
-// import Content from "./components/Content";
-// import Total from "./components/Total";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
+  const generateCourses = () =>
+    courses.map(course => <Course key={course.id} course={course} />);
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web Development Curriculum</h1>
+      {generateCourses()}
     </div>
   );
 };
