1631a132159bb4825a4147dced1b853810eac957
Chris <Chris.haggert@gmail.com>
2019-10-03 16:04:39 -0400

    finished 2.9

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
-  const contacts = () =>
-    persons.map(person => (
-      <p key={person.name}>
-        {person.name} {person.number}
-      </p>
-    ));
+  const contacts = contactFilter =>
+    persons
+      .filter(person => person.name.includes(contactFilter))
+      .map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ));
 
   const addContact = event => {
     event.preventDefault();
@@ -38,22 +44,31 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search contacts:{" "}
+        <input value={filter} onChange={handleFilterChange}></input>
+      </div>
+      <h2>Add New Contact</h2>
       <form onSubmit={addContact}>
         <div>
-          name: <input value={newName} onChange={handleContactChange} />
+          Name: <input value={newName} onChange={handleContactChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {contacts()}
+      {contacts(filter)}
     </div>
   );
 };
