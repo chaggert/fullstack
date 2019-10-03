dbb66b90f4a2b903b342857574dea27ce2565d33
Chris <Chris.haggert@gmail.com>
2019-10-02 21:11:15 -0400

    finished 2.6

---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const contacts = () =>
+    persons.map(person => <p key={person.name}>{person.name}</p>);
+
+  const addContact = event => {
+    event.preventDefault();
+    const contactObject = {
+      name: newName
+    };
+    setPersons(persons.concat(contactObject));
+    setNewName("");
+  };
+
+  const handleContactChange = event => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={handleContactChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {contacts()}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
