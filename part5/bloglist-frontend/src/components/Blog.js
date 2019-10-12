3f53dc87be8402ef727516cb41af63d98dc38023
Chris <Chris.haggert@gmail.com>
2019-10-12 16:58:53 -0400

    finished 5.6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+  };
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setShowDetails(!showDetails)}>
+        {blog.title} {blog.author}
+      </div>
+      {showDetails ? (
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+          <p>
+            {blog.likes} likes <button>like!</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
