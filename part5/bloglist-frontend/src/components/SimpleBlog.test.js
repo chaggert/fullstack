4acbda4b0e980e0c3ba430079b2ac7bb34ce5851
Chris <Chris.haggert@gmail.com>
2019-10-14 18:05:57 -0400

    finished 5.14

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import SimpleBlog from "./SimpleBlog";
+import { likeClickHandler } from "./Blog";
 
 test("renders content", () => {
   const blog = {
@@ -22,4 +23,24 @@ test("renders content", () => {
   );
   expect(component.container).toHaveTextContent("The Author");
   expect(component.container).toHaveTextContent("blog has 2 likes");
+});
+
+test("clicking like twice calls the event handler twice", () => {
+  const blog = {
+    title: "Component testing is done with react-testing-library",
+    author: "The Author",
+    url: "www.google.com",
+    likes: 2
+  };
+
+  const mockHandler = jest.fn();
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  );
+  const button = getByText("like");
+  fireEvent.click(button);
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls.length).toBe(2);
 });
