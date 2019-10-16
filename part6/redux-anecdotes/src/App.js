51514d4a945c1809ba5227c85ece7e946f3c3679
Chris <Chris.haggert@gmail.com>
2019-10-16 15:18:44 -0400

    finished 6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -14,19 +14,22 @@ const App = props => {
   const vote = id => {
     props.store.dispatch(voteFor(id));
   };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+      {anecdotes
+        .sort(function(a, b) {
+          return b.votes - a.votes;
+        })
+        .map(anecdote => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div>
