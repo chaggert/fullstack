99ce1e822bf547da51fd978745e8cb0c1b927bc3
Chris <Chris.haggert@gmail.com>
2019-10-17 22:56:18 -0400

    finished 6.18

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,37 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
-import anecdoteReducer, {
-  initializeAnecdotes
-} from "./reducers/anecdoteReducer";
-import notificationReducer from "./reducers/notificationReducer";
-import filterReducer from "./reducers/filterReducer";
-import anecdoteService from "./services/anecdotes";
+import store from "./store";
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-});
-
-const store = createStore(reducer);
-
-anecdoteService
-  .getAll()
-  .then(anecdotes => store.dispatch(initializeAnecdotes(anecdotes)));
-
-// store.subscribe(() => console.log(store.getState()));
-
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById("root")
-  );
-};
-
-render();
-store.subscribe(render);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from "../services/anecdotes";
+
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 export const asObject = anecdote => {
@@ -29,10 +31,13 @@ const anecdoteReducer = (state = [], action) => {
   }
 };
 
-export const initializeAnecdotes = anecdotes => {
-  return {
-    type: "INIT_ANECDOTES",
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: "INIT_ANECDOTES",
+      data: anecdotes
+    });
   };
 };
 
